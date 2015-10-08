49f4d783dea52ba2175e1326f3df2b202c54a9df
Joe Hand <joe@joeahand.com>
2015-10-08 15:28:50 -0600

    separate map styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,37 +1,4 @@
 
-var DEFAULT_STYLES = {
-                weight: 2,
-                color: '#FFA500',
-                opacity: 0.8,
-                fillColor: '#B0DE5C',
-                fillOpacity: 0.8,
-                dashArray: '4'
-            };
-
-var INNER_STYLES = {
-                weight: 2.5,
-                color: '#FFA500',
-                opacity: 0.8,
-                fillColor: '#blue',
-                fillOpacity: 0.8
-             };
-
-var ORIGINAL_ROAD_STYLES = {
-                weight: 2.5,
-                color: '#2F4F4F',
-                opacity: 0.7,
-                fillColor: '#B0DE5C',
-                fillOpacity: 0.7
-            };
-
-var NEW_ROAD_STYLES = {
-                weight: 2.5,
-                color: '#2F4F4F',
-                opacity: 0.8,
-                fillColor: '#B0DE5C',
-                fillOpacity: 0.8
-            };
-
 var map = L.map('map', { zoomControl: false });
 
 var tiles = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
