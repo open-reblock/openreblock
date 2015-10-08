17437eeb8cbd3585faf6bbf385b16bb583428bc5
Joe Hand <joe@joeahand.com>
2015-10-08 15:07:36 -0600

    project cape town data and add

---
+++ data/capetown/stats.js
@@ -0,0 +1,19 @@
+var projectData = {
+    totalSteps: 2,
+    steps: [
+        {
+            file: 'data/capetown/Cape_Step0.json',
+            pathArea: 50,
+            parcelArea: 10000,
+            pathPercent: 12,
+            isolatedParcels: 7
+        },
+        {
+            file: 'data/capetown/Cape_Step0.json',
+            pathArea: 450,
+            parcelArea: 8000,
+            pathPercent: 25,
+            isolatedParcels: 0
+        }
+    ]
+};
